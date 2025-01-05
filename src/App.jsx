cfd80f6e4532c3cdf49c2c452cb1935ebf7eecb2
ArshdeepSingh <deepsingh13131212@gmail.com>
2025-01-05 18:19:26 +0530

    almost done with appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import { useEffect, useState } from 'react'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading ,setLoading]= useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Welcome to Blogs 🥳</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    }).finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+  return !loading?(
+    <div className='main-h-screen flex flex-wrap content-between bg-gray-400'>
+      <Header/>
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  ):null
 }
 
 export default App
